a7f9abf1bf9a41e858d3f83ed0a25aee8627ecff
Akileshwaran <67944263+ak2205@users.noreply.github.com>
2022-02-01 18:19:42 +0530

    Add files via upload

---
+++ Perimeter-of-circle.py
@@ -0,0 +1,14 @@
+pi=3.14
+r=float(input("Enter value of radius"))
+h=float(input("Enter the height"))
+
+area_circle = int(pi*r*r)
+print("Area of a circle =", area_circle)
+
+perimeter = int(2*pi*r)
+print("Perimeter of circle is = ", perimeter)
+
+vol_cone = int(0.33*pi*r*r*h)
+print("Volume of a cone is =", vol_cone)
+
+
